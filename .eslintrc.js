dc51694e1a10b8f04767e427e70f78b9ea75c7fb
Patrick Morgan <patrick@jpsignum.com>
2025-01-28 18:54:15 -0500

    started to build out auth skeleton, adjusted configurations to ensure natiewind classNames were being picked up correctly, adjusted styling to natiewind, set up splash screen loading barrier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,24 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
+  root: true,
+  extends: ["eslint:recommended", "expo", "prettier"],
   plugins: ["prettier"],
   rules: {
     "prettier/prettier": "warn",
+
+    // code quality rules
+    "no-unused-vars": "warn",
+    "no-console": "warn",
+    "import/no-unresolved": "error",
+    "react/jsx-key": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,27 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
 
-import "./global.css"
+import "../styles/global.css";
+
+// prevent the splash screen from auto-hiding before asset
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <Stack />;
 }
