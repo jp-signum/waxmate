13d25c19e152614dd16f1f8caad717509e666260
Patrick Morgan <patrick@jpsignum.com>
2025-01-31 21:55:41 -0500

    continued skeleton by implementing tab layout naviagtion for home and identify screens

---
+++ app/(tabs)/identify.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Identify() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-base">Identify Placeholder</Text>
+    </View>
+  );
+}
